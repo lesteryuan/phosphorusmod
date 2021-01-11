9c288069941d0bd3185f69869fac85b6a4f09e33
lesteryuan <58045380+lesteryuan@users.noreply.github.com>
2021-01-11 15:38:40 -0500

    added plots to chlvss

--- chlvssmod.R
+++ chlvssmod.R
@@ -30,6 +30,11 @@ chlvssmod <- function(df1,varout = NULL,
     df1$lake <- factor(df1$lake)
     df1$lakenum <- as.numeric(df1$lake)
 
+    print(max(df1$lakenum))
+    print(nrow(df1))
+    print(table(df1$year))
+    stop()
+
     ## scale variables
     varlist<- c("vss", "chl")
     mn.val <- apply(df1[, varlist],2,function(x) exp(mean(log(x))))
@@ -82,6 +87,7 @@ chlvssmod <- function(df1,varout = NULL,
     '
     rmsout <- function(x,y) sqrt(sum((x-y)^2)/length(x))
 
+    grey.t <- adjustcolor("grey40", alpha.f = 0.5)
     extractvars <- c("k", "mub", "u1")
 
     if (runmod) {
@@ -104,12 +110,31 @@ chlvssmod <- function(df1,varout = NULL,
 
     }
 
-    k <- mean(varout$k)
-    mub <- mean(varout$mub)
-    dev.new()
-    par(mar = c(4,4,1,1), mfrow = c(1,2))
-    plot(log(df1$chl), log(df1$vss), col = "grey")
-    abline(mub, k)
+
+    x <- seq(min(log(df1$chl)), max(log(df1$chl), length = 50))
+    xraw <- x+log(mn.val["chl"])
+    predout <- matrix(NA, ncol = 3, nrow = length(x))
+    for (i in 1:length(x)) {
+        y <- x[i]*varout$k + varout$mub + log(mn.val["vss"])
+        predout[i,] <- quantile(y, prob = c(0.05, 0.5, 0.95))
+    }
+    png(width = 3, height = 2.5, pointsize = 6, units = "in", res = 600,
+        file = "vsschlplot.png")
+    par(mar = c(4,4,1,1), mfrow = c(1,1), mgp = c(2.3,1,0))
+    plot(log(df1$chl*mn.val["chl"]), log(df1$vss*mn.val["vss"]),
+         pch = 21, col = "grey", bg = "white",
+         xlab = expression(Chl~(mu*g/L)),
+         ylab = "VSS (mg/L)", axes = F)
+    logtick.exp(0.001, 10, c(1,2), c(F,F))
+    polygon(c(xraw, rev(xraw)), c(predout[,1], rev(predout[,3])), border = NA,
+            col = grey.t)
+    lines(xraw, predout[,2])
+    dev.off()
+
+    print(quantile(mn.val["vss"]*exp(varout$mub)/(mn.val["chl"]^varout$k),
+                   prob = c(0.05, 0.5, 0.95)))
+    print(quantile(varout$k, prob = c(0.05, 0.5, 0.95)))
+    stop()
 
     df2 <- merge(df1, res.dat, by.x = "lake", by.y = "MU..")
     df2 <- unique.data.frame(df2[, c("lake", "lakenum", "flush.rate", "mean.depth")])
@@ -123,4 +148,4 @@ chlvssmod <- function(df1,varout = NULL,
 
 }
 #fitout <- chlvssmod(mo.all, runmod = T)
-chlvssmod(mo.all, varout, runmod= F)
+chlvssmod(mo.all, varout.vss, runmod= F)
